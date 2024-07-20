44f16ab007f151651596cbd503ffc4efe64b8ae0
ridham <108151918+RidhamRupavatiya@users.noreply.github.com>
2024-07-21 00:16:19 +0530

    first commit

--- LeetCode/DCC July 2024/FindTheValidMatrix.java
+++ LeetCode/DCC July 2024/FindTheValidMatrix.java
@@ -25,7 +25,7 @@ class FindTheValidMatrix{
     public static void main(String[] args) {
         FindTheValidMatrix obj = new FindTheValidMatrix();
         int rowSum[] = {3,8};
-        int colSum[] = {4,7}; 
+        int colSum[] = {4,7};
         int ans[][] = obj.restoreMatrix(rowSum, colSum);
         for (int i = 0; i < ans.length; i++) {
             for (int j = 0; j < ans[0].length; j++) {
@@ -34,6 +34,37 @@ class FindTheValidMatrix{
         }
     }
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        
+        int m = rowSum.length;
+        int n = colSum.length;
+        int[][] mat = new int[m][n];
+
+        Set<Integer> rSet = new HashSet<>();
+        Set<Integer> cSet = new HashSet<>();
+
+        while(rSet.size() != m && cSet.size() != n){
+            int ri = minInd(rowSum, rSet);
+            int ci = minInd(colSum, cSet);
+            if(rowSum[ri] < colSum[ci]){
+                mat[ri][ci] = rowSum[ri];
+                colSum[ci] -= rowSum[ri];
+                rSet.add(ri);
+            } else{
+                mat[ri][ci] = colSum[ci];
+                rowSum[ri] -= colSum[ci];
+                cSet.add(ci);
+            }
+        }
+        return mat;
+    }
+    private int minInd(int[] a, Set<Integer> set){
+        int min = Integer.MAX_VALUE;
+        int ind = 0;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] < min && !set.contains(i)){
+                min = a[i];
+                ind = i;
+            }
+        }
+        return ind;
     }
 }
